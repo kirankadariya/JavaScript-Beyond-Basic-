3689a4adedc9f48820ea1bd859f6487437eb4534
kirankadariya <kirankadariya@gmail.com>
2023-11-27 11:54:59 -0600

    commit to chain

---
+++ chaining.js
@@ -0,0 +1,24 @@
+
+
+const people = [
+  { name: "Alice", age: 30 },
+  { name: "Hope", age: 19 },
+  { name: "Ben", age: 25 },
+  { name: "Charlie", age: 50 },
+  { name: "Dana", age: 28 },
+  { name: "Joey", age: 43 },
+];
+
+const filterPeople = people 
+                    .filter(person => person.age >= 40)  
+                    .map(person =>person.name);
+                    
+    console.log(filterPeople);
+
+const sum= numbers
+            .map(num => num * 2)
+            .filter(num =>  num <=6)
+            .reduce((acc, num) => sum= acc + num);
+
+
+console.log(sum);
