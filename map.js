686e4a8e6f93983300ce71580a3e8e802b12d0a8
kirankadariya <kirankadariya@gmail.com>
2023-11-27 11:23:28 -0600

    updat2

--- map.js
+++ map.js
@@ -3,8 +3,7 @@ const numbers = [1, 2, 3, 4, 5];
 const books = [
   { title: "The Catcher in the Rye", author: "J.D. Salinger" },
   { title: "The Art of Gathering", author: "Priya Parker" },
-  { title: "1984", author: "George Orwell" },
-  { title: "The Great Gatsby", author: "F. Scott Fitzgerald" },
+  { title: "1984", author: "George Orwell" },  { title: "The Great Gatsby", author: "F. Scott Fitzgerald" },
   { title: "Atomic habits", author: "James Clear" },
 ];
 
@@ -13,10 +12,13 @@ const names = ["abra", "bo", "guil", "hanna", "jacob", "kayla"];
 const multipliedNumbers = numbers.map(function (num) {
   return num * 2;
 });
+console.log(multipliedNumbers)
+
 
 const bookTitles = books.map(function (book, i) {
   return i + " - " + book.author;
 });
+console.log(bookTitles)
 
 const capitalizedNames = names.map(function (name) {
   return name.charAt(0).toUpperCase() + name.slice(1);
